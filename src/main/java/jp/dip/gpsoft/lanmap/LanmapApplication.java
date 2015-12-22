8ec93c8da23dc83cd4fd9421a995fbf6fa71f71e
gpsoft <etoh.gpsoft@gmail.com>
2015-12-22 12:00:08 +0900

    init

---
+++ src/main/java/jp/dip/gpsoft/lanmap/LanmapApplication.java
@@ -0,0 +1,12 @@
+package jp.dip.gpsoft.lanmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanmapApplication.class, args);
+    }
+}
---
+++ src/test/java/jp/dip/gpsoft/lanmap/LanmapApplicationTests.java
@@ -0,0 +1,18 @@
+package jp.dip.gpsoft.lanmap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = LanmapApplication.class)
+@WebAppConfiguration
+public class LanmapApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
