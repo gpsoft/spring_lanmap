8a016fe275174e5000b15f72ed0267da7953a730
gpsoft <etoh.gpsoft@gmail.com>
2015-12-23 17:11:09 +0900

    create user

--- src/main/java/jp/dip/gpsoft/lanmap/controller/UsersController.java
+++ src/main/java/jp/dip/gpsoft/lanmap/controller/UsersController.java
@@ -3,8 +3,12 @@ package jp.dip.gpsoft.lanmap.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import jp.dip.gpsoft.lanmap.form.UserForm;
+import jp.dip.gpsoft.lanmap.model.User;
 import jp.dip.gpsoft.lanmap.service.UserService;
 
 @Controller
@@ -17,6 +21,19 @@ public class UsersController extends BaseController {
 
 		model.addAttribute("users", userService.findAll());
 		return "users/index";
+	}
+	
+	@RequestMapping("/users/create")
+	public String create(@ModelAttribute UserForm form) {
+		return "users/form";
+	}
+	
+	@RequestMapping(value="/users/save", method=RequestMethod.POST)
+	public String save(@ModelAttribute UserForm form, Model model) {
+		User user = new User();
+		user.setName(form.getName());
+		userService.save(user, form.getPassword());
+		return "redirect:/users";
 	}
 
 }
--- src/main/java/jp/dip/gpsoft/lanmap/service/UserService.java
+++ src/main/java/jp/dip/gpsoft/lanmap/service/UserService.java
@@ -1,8 +1,10 @@
 package jp.dip.gpsoft.lanmap.service;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,7 +18,21 @@ public class UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	// SecurityConfigの中で@Beanメソッドを定義してる。
+
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public void save(User user, String password) {
+		user.setPassword(passwordEncoder.encode(password));
+		Timestamp now = new Timestamp(System.currentTimeMillis());
+		if (user.getCreated() == null) {
+			user.setCreated(now);
+		}
+		user.setModified(now);
+		userRepository.saveAndFlush(user);
 	}
 }
