0cabda17bf6bb4830d385b63d3f37a0d7dec92f0
gpsoft <etoh.gpsoft@gmail.com>
2016-01-11 12:09:15 +0900

    download file

--- src/main/java/jp/dip/gpsoft/lanmap/controller/NodesController.java
+++ src/main/java/jp/dip/gpsoft/lanmap/controller/NodesController.java
@@ -1,9 +1,17 @@
 package jp.dip.gpsoft.lanmap.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import jp.dip.gpsoft.lanmap.service.NodeService;
 
@@ -17,5 +25,12 @@ public class NodesController extends BaseController {
 
 		model.addAttribute("nodes", nodeService.findAllAlive());
 		return "nodes/index";
+	}
+
+	@RequestMapping(value="/nodes/download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public Resource download(HttpServletRequest request, HttpServletResponse response) {
+		response.setHeader(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=download.xml");
+		return new FileSystemResource("pom.xml");
 	}
 }
